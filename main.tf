6c8b3cf2736106114c3b6d46a6f69da0ffaf3c32
Narendra Kumar Vadapalli <narenandu@gmail.com>
2023-09-25 19:49:40 -0700

    Initial addition of initialized terraform module this creates only a resource group

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.90.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main"{
+    name = "learn-tf-rg-eastus"
+    location = "eastus"
+}
